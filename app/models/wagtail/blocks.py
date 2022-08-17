3cce8eb66fcce3615bbc593f1ff5e0524d89757d
Jan Baykara <janbaykara@pm.me>
2022-08-17 11:45:44 +0100

    Add Shorthand embed example

--- app/models/wagtail/blocks.py
+++ app/models/wagtail/blocks.py
@@ -6,3 +6,14 @@ class PageSummaryBlock(blocks.StructBlock):
 
     class Meta:
         template = "app/blocks/page_summary_block.html"
+
+
+class HTMLBlock(blocks.StructBlock):
+    description = blocks.RichTextBlock(
+        help_text="(Internal use only.) Explain what this does for other editors. Not displayed on the website.",
+        required=False,
+    )
+    html = blocks.CharBlock(max_length=10000)
+
+    class Meta:
+        template = "app/blocks/html_block.html"
--- app/models/wagtail/pages.py
+++ app/models/wagtail/pages.py
@@ -1,6 +1,7 @@
+from django.db import models
 from wagtail import blocks
-from wagtail.admin.panels import FieldPanel
-from wagtail.fields import StreamField
+from wagtail.admin.panels import FieldPanel, ObjectList, TabbedInterface
+from wagtail.fields import RichTextField, StreamField
 from wagtail.models import Page
 
 import app.models.wagtail.blocks as app_blocks
@@ -42,15 +43,32 @@ class HomePage(Page):
 
 class StaticPage(Page):
     show_in_menus_default = True
+
+    # Content
     layout = StreamField(
         [
             ("richtext", blocks.RichTextBlock()),
+            ("embeddable_code", app_blocks.HTMLBlock()),
         ],
         null=True,
         blank=True,
         use_json_field=True,
     )
     content_panels = Page.content_panels + [FieldPanel("layout")]
+
+    # Layout
+    show_header = models.BooleanField(default=True)
+    show_footer = models.BooleanField(default=True)
+    layout_panels = [FieldPanel("show_header"), FieldPanel("show_footer")]
+
+    edit_handler = TabbedInterface(
+        [
+            ObjectList(content_panels, heading="Content"),
+            ObjectList(layout_panels, heading="Layout"),
+            ObjectList(Page.promote_panels, heading="Promote"),
+            ObjectList(Page.settings_panels, heading="Settings", classname="settings"),
+        ]
+    )
 
 
 class MagazineIndexPage(Page):
@@ -69,9 +87,11 @@ class MagazineIndexPage(Page):
 class MagazineArticlePage(Page):
     parent_page_type = ["app.MagazineIndexPage"]
     show_in_menus_default = True
+    article = RichTextField()
     layout = StreamField(
         [
             ("richtext", blocks.RichTextBlock()),
+            ("embeddable_code", app_blocks.HTMLBlock()),
         ],
         null=True,
         blank=True,
