c81221a54836497cf6d7739c19dfc9b20f286ae5
Anna Cunnane <acunnane13@gmail.com>
2022-11-09 12:49:56 +0000

    add stimulus carousel controller

---
+++ frontend/controllers/carousel-controller.ts
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus";
+import { SwiperOptions } from "swiper";
+// @ts-ignore
+import Swiper from "swiper/bundle";
+
+export default class extends Controller {
+    swiper: Swiper;
+    // @ts-ignore
+    optionsValue: SwiperOptions;
+
+    static values = {
+        options: Object,
+    };
+
+    connect(): void {
+        this.swiper = new Swiper(this.element, {
+            ...this.defaultOptions,
+            ...this.optionsValue,
+        });
+    }
+
+    disconnect(): void {
+        this.swiper.destroy();
+        this.swiper = undefined;
+    }
+
+    get defaultOptions(): SwiperOptions {
+        return {};
+    }
+}
