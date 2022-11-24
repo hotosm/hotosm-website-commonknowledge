ec8d2a96b7459b0b4883dd63c7990fe3bf3c8ce0
Jan Baykara <janbaykara@pm.me>
2022-11-24 14:42:20 +0000

    Handle weirdly formatted nested lists in frontmatter YAML -> JSON conversion

--- app/management/commands/geocode_pages.py
+++ app/management/commands/geocode_pages.py
@@ -5,7 +5,7 @@ from django.core.management.base import BaseCommand
 from django.db import transaction
 
 from app.models import CountryPage
-from app.utils.python import ensure_list
+from app.utils.python import ensure_1D_list
 from app.views.api import MapSearchViewset
 
 
@@ -24,36 +24,47 @@ class Command(BaseCommand):
                     frontmatter = page.frontmatter
                     if isinstance(page.frontmatter, str):
                         frontmatter = json.loads(page.frontmatter)
-                    listed_countries = ensure_list(frontmatter.get("Country", list()))
-                    country_pages = []
-                    for listed_country in listed_countries:
-                        if listed_country is not None and len(listed_country) > 0:
-                            try:
-                                results = pycountry.countries.search_fuzzy(
-                                    listed_country
-                                )
-                                if len(results) != 0:
-                                    metadata = results[0]
-                                    country_instance = country_code_database_map.get(
-                                        metadata.alpha_2, None
+                    listed_countries = ensure_1D_list(
+                        frontmatter.get("Country", list())
+                    )
+                    if len(listed_countries) > 0:
+                        country_pages = []
+                        for listed_country in listed_countries:
+                            if (
+                                listed_country is None
+                                or not isinstance(listed_country, str)
+                                or len(listed_country) == 0
+                            ):
+                                try:
+                                    results = pycountry.countries.search_fuzzy(
+                                        listed_country
                                     )
-                                    if country_instance is None:
-                                        country_instance = CountryPage.create_for_code(
-                                            metadata.alpha_2
+                                    if len(results) != 0:
+                                        metadata = results[0]
+                                        country_instance = (
+                                            country_code_database_map.get(
+                                                metadata.alpha_2, None
+                                            )
                                         )
-                                        country_root_page.add_child(
-                                            instance=country_instance
-                                        )
-                                        country_code_database_map[
-                                            metadata.alpha_2
-                                        ] = country_instance
-                                    if country_instance is not None:
-                                        country_pages.append(country_instance)
-                            except LookupError:
-                                pass
-                    if len(country_pages) > 0:
-                        page.related_countries.add(*country_pages)
-                        revision = page.save_revision()
-                        if page.live:
-                            page.publish(revision)
-                    country_pages = []
+                                        if country_instance is None:
+                                            country_instance = (
+                                                CountryPage.create_for_code(
+                                                    metadata.alpha_2
+                                                )
+                                            )
+                                            country_root_page.add_child(
+                                                instance=country_instance
+                                            )
+                                            country_code_database_map[
+                                                metadata.alpha_2
+                                            ] = country_instance
+                                        if country_instance is not None:
+                                            country_pages.append(country_instance)
+                                except LookupError:
+                                    pass
+                        if len(country_pages) > 0:
+                            page.related_countries.add(*country_pages)
+                            revision = page.save_revision()
+                            if page.live:
+                                page.publish(revision)
+                        country_pages = []
--- app/utils/python.py
+++ app/utils/python.py
@@ -2,3 +2,14 @@ def ensure_list(el_or_list):
     if isinstance(el_or_list, list):
         return el_or_list
     return [el_or_list]
+
+
+def ensure_1D_list(el_or_list):
+    l = ensure_list(el_or_list)
+    new_l = []
+    for el in l:
+        if isinstance(el, list):
+            new_l += ensure_1D_list(el)
+        else:
+            new_l += [el]
+    return new_l
